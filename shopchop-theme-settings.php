a546eb8bd23f6f4be7a04470d1a26f565e344e90
usoppii <muhdyusuffrosli@gmail.com>
2026-08-05 10:51:10 +0800

    chore: rename plugin to The Splash Shop Settings, bump to 1.2.1

--- shopchop-theme-settings.php
+++ shopchop-theme-settings.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:       ShopChop Theme Settings
+ * Plugin Name:       The Splash Shop Settings
  * Plugin URI:        https://github.com/AquariusSPoolsDev/SplashShop-WC-Theme-Settings
- * Description:       Custom Elementor widgets and theme extensions for ShopChop.
- * Version:           1.2.0
+ * Description:       Custom Elementor widgets and other settings for The Splash Shop.
+ * Version:           1.2.1
  * Requires at least: 6.0
  * Requires PHP:      8.0
  * Author:            Usoppii
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SHOPCHOP_SETTINGS_VERSION', '1.2.0' );
+define( 'SHOPCHOP_SETTINGS_VERSION', '1.2.1' );
 define( 'SHOPCHOP_SETTINGS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SHOPCHOP_SETTINGS_URL', plugin_dir_url( __FILE__ ) );
 
